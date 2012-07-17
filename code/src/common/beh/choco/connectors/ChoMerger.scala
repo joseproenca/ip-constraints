a912b27a52c08a398e192a455efeb6be633143d7
jose <jose.proenca@cs.kuleuven.be>
2012-07-17 10:24:52 +0200

    Missing files.

---
+++ code/src/common/beh/choco/connectors/ChoMerger.scala
@@ -0,0 +1,38 @@
+package common.beh.choco.connectors
+
+import common.beh.Utils._
+import common.beh.choco._
+
+
+//import common.beh.ConnectorBeh
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jose
+ * Date: 30/04/12
+ * Time: 13:56
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ChoMerger(x:String,y:String,z:String,uid:Int) extends ChoBehaviour(List(x,y),uid) {
+
+  useData = false
+  useCC3 = false
+
+  val (xv,yv,zv) = (Var(flowVar(x,uid)) , Var(flowVar(y,uid)), Var(flowVar(z,uid)))
+
+  //(z <-> (x \/ y))  /\ !(a/\b)
+  var constraints = ChoConstraints(List(
+    zv <-> (xv or yv),
+    Neg(xv and yv)
+  ))
+
+  // suggests which ends must have dataflow if "end" has also dataflow
+//  def guessRequirements(end: String) = end match {
+//    case `x` => Set(z)
+//    case `y` => Set(z)
+//    case `z` => Set(x) // priority to x
+//    case _ => Set()
+//  }
+}
+
---
+++ code/src/common/beh/choco/connectors/ChoSync.scala
@@ -0,0 +1,24 @@
+package common.beh.choco.connectors
+
+import common.beh.Utils._
+import common.beh.choco._
+
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jose
+ * Date: 02/05/12
+ * Time: 09:57
+ * To change this template use File | Settings | File Templates.
+ */
+
+class ChoSync(x:String,y:String,uid:Int) extends ChoBehaviour(List(x,y),uid) {
+
+  useData = false
+  useCC3 = false
+
+  var constraints = ChoConstraints(VarEq(flowVar(x,uid),flowVar(y,uid)))
+
+  // suggests which ends must have dataflow if "end" has also dataflow
+//  def guessRequirements(end: String) = if (end == x) Set(y) else Set(x)
+}
