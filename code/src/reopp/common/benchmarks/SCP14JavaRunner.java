486cdd3808b8450f4ad0fb479f1752b2fac4b753
Jose Proenca <jose.proenca@cs.kuleuven.be>
2014-03-18 12:24:38 +0100

    Renaming the java runner for the benchmarks.

--- code/src/reopp/common/benchmarks/SCP14JavaRunner.java
+++ code/src/reopp/common/benchmarks/SCP14JavaRunner.java
@@ -1,6 +1,6 @@
 package reopp.common.benchmarks;
 
-public class ScalaRunner {
+public class SCP14JavaRunner {
     public static void main(String[] args) {
 		reopp.common.benchmarks.SCP14.main(args);
     }
